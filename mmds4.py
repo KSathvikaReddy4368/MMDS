ccbd8c4cbebb17a612af92376929431240ccf46d
KSathvikaReddy4368 <22hr1a3027@mtieat.org>
2024-10-29 10:33:04 +0530

    mmds4.py

---
+++ mmds4.py
@@ -0,0 +1,12 @@
+import networkx as nx
+# import matplotlib.pyplot library
+import matplotlib.pyplot as plt
+# import networkx library
+import networkx as nx
+# create a cubical empty graph
+G = nx.cubical_graph()
+# plotting the graph
+plt.subplot(122)
+# draw a graph with red
+# node and value edge color
+nx.draw(G, pos = nx.circular_layout(G),node_color = 'r',edge_color = 'b')
